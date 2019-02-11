2f87cd7dcba20eaa51c726d3af21e09fc45e1d76
Olavi Mustanoja <tilastokeskus@gmail.com>
2019-02-11 03:40:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+A simple command line tool for converting sass/scss to jss.
+
+### Install
+
+```
+$ npm install -g sass2jss
+```
+
+or
+
+```
+$ yarn global add sass2jss
+```
+
+### Usage
+
+```
+$ sass2jss -f input.scss -o output.js
+```
+
+You can omit the output file argument, in which case the resulting jss will be printed to stdout.
---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const args = require('args');
+const sass = require('node-sass');
+const cssToJss = require('jss-cli').cssToJss;
+const prettier = require('prettier');
+
+args
+	.option('file', 'Sass file to convert to jss (required)')
+	.option('outputFile', 'File to save resulting jss to');
+
+const flags = args.parse(process.argv);
+
+if (!flags.file) {
+	args.showHelp();
+	process.exit(1);
+}
+
+const res = sass.renderSync({
+	file: path.resolve(process.cwd(), flags.file)
+});
+
+const js = cssToJss({code: res.css.toString('utf-8')});
+const styles = js['@global'];
+
+const str = `const styles = ${JSON.stringify(styles)}`;
+const formattedStr = prettier.format(str, {singleQuote: true, useTabs: true, parser: 'babel'});
+
+if (flags.outputFile) {
+	fs.writeFileSync(path.resolve(process.cwd(), flags.outputFile), formattedStr);
+} else {
+	console.log(formattedStr);
+}
